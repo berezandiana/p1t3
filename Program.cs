b161a5af0867e5efa72191a7ce7e23651e30097a
Berezan Diana <119860630+berezandiana@users.noreply.github.com>
2023-04-27 12:43:01 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введiть речення:");
+        string sentence = Console.ReadLine();
+
+        int maxSpaces = 0;
+        int currentSpaces = 0;
+
+        for (int i = 0; i < sentence.Length; i++)
+        {
+            if (sentence[i] == ' ')
+            {
+                currentSpaces++;
+                if (currentSpaces > maxSpaces)
+                {
+                    maxSpaces = currentSpaces;
+                }
+            }
+            else
+            {
+                currentSpaces = 0;
+            }
+        }
+
+        Console.WriteLine("Найбiльша кiлькiсть пробiлiв, що розташованi пiдряд: " + maxSpaces);
+        Console.ReadLine();
+    }
+}
